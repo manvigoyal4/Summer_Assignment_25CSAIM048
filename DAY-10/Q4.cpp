a9892c2db51dd5cdf0de1bbc34339fbe07d780a8
manvigoyal4 <manviigoyal4@gmail.com>
2026-06-10 22:30:02 +0530

    Day 10 done

---
+++ DAY-10/Q4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    //WAP to print chracter pyramid pattern.
+    int i,j,n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++){
+            printf("%c",65+j-1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
